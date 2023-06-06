b4a01e16c353b24ef2a97dd1c9ff6b4ab7ec6fb9
Carlos Adriano Miranda <adriadrianooo@gmail.com>
2023-06-06 18:47:34 -0300

    chore: configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  appDir: true,
+  serverComponentsExternalPackages: ["mongoose"],
+  serverRuntimeConfig: {
+      connectionString: "mongodb://localhost/next-js-registration-login-example",
+      secret: 'Ifbt@iy7'
+  }
+}
 
 module.exports = nextConfig
