70953ac352f2428c1637b2b4e590bd67bca03f5c
Carlos Adriano Miranda <adriadrianooo@gmail.com>
2023-06-10 15:57:32 -0300

    throw error

--- src/query.ts
+++ src/query.ts
@@ -4,7 +4,14 @@ const queries = {
   queryAllWishItems: (): Promise<WishItemType[]> => {
     return fetch(
       `/api/wishlist/getall`
-    ).then((res) => res.json());
+    ).then((res) => {
+      if (res.status === 200) {
+
+        return res.json();
+      }
+
+      throw (res);
+    });
   },
   addWishItem: (body: Omit<WishItemType, "value"> & { value: string }) => {
     return fetch(`/api/wishlist/add`, {
