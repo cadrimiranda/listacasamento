b98234900f707325b0ab790a7698ea00f5eed213
Carlos Adriano Miranda <adriadrianooo@gmail.com>
2023-06-10 18:10:50 -0300

    loading when fetching all

---
+++ app/private/wishlist/add/SubmitButton.tsx
@@ -0,0 +1,22 @@
+import { MouseEventHandler } from "react";
+import styles from "./page.module.css";
+
+type ButtonProps = {
+  isLoading: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+const SubmitButton = ({ isLoading, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`${styles.submitButton} ${isLoading ? styles.loading : ""}`}
+      onClick={onClick}
+      disabled={isLoading}
+      type="submit"
+    >
+      {isLoading ? "" : "Submit"}
+    </button>
+  );
+};
+
+export default SubmitButton;
--- components/WishList.tsx
+++ components/WishList.tsx
@@ -1,34 +1,72 @@
 "use client";
-import { useEffect, useState } from "react";
+import {
+  Suspense,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from "react";
 import WishItem, { WishItemType } from "./WishItem";
 import styles from "../app/page.module.css";
+import wishItemStyle from "./WishItem.module.css";
 import queries from "@/src/query";
 
-const WishList = ({ shouldDelete }: { shouldDelete?: boolean }) => {
-  const [itens, setItens] = useState<WishItemType[]>([]);
-
-  const getData = () => {
-    queries.queryAllWishItems().then((res) => {
-      setItens(res);
-    });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return (
-    <div className={styles.listaPresentes}>
-      {itens.map((props) => (
-        <WishItem
-          refreshData={getData}
-          shouldDelete={shouldDelete}
-          key={props.title}
-          {...props}
-        />
-      ))}
-    </div>
-  );
+export type WishListRef = {
+  getData: () => void;
 };
+
+const WishList = forwardRef<WishListRef, { shouldDelete?: boolean }>(
+  ({ shouldDelete }, ref) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [itens, setItens] = useState<WishItemType[]>([]);
+
+    const getData = useCallback(() => {
+      setIsLoading(true);
+      queries.queryAllWishItems().then((res) => {
+        setItens(res);
+        setIsLoading(false);
+      });
+    }, [setItens]);
+
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          getData,
+        };
+      },
+      [getData]
+    );
+
+    useEffect(() => {
+      getData();
+    }, []);
+
+    return (
+      <Suspense>
+        <div className={styles.listaPresentes}>
+          {isLoading && (
+            <>
+              {[1, 2, 3, 4].map((value) => (
+                <div key={value} className={styles.skeletonBox} />
+              ))}
+            </>
+          )}
+          {itens.map((props) => (
+            <WishItem
+              refreshData={getData}
+              shouldDelete={shouldDelete}
+              key={props.title}
+              {...props}
+            />
+          ))}
+        </div>
+      </Suspense>
+    );
+  }
+);
+
+WishList.displayName = "WishList";
 
 export default WishList;
